f2818970bb7719c43d4bb43ed2da9fd8440abda8
rohgoon <rohgoon@gmail.com>
2016-12-19 11:34:56 +0900

    필요없는 구문 정리

--- src/kr/or/dgit/bigdata/coffee/ui/list/CoffeeList1.java
+++ src/kr/or/dgit/bigdata/coffee/ui/list/CoffeeList1.java
@@ -1,22 +1,6 @@
 package kr.or.dgit.bigdata.coffee.ui.list;
 
-import java.awt.BorderLayout;
-import java.awt.Font;
-import java.util.List;
 
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
-import javax.swing.SwingConstants;
-import javax.swing.border.EmptyBorder;
-import javax.swing.table.DefaultTableCellRenderer;
-import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableColumnModel;
-
-import kr.or.dgit.bigdata.coffee.dao.TableDao;
-import kr.or.dgit.bigdata.coffee.dto.ViewTable;
-import java.awt.GridLayout;
 
 public class CoffeeList1 extends CoffeeList {
 
--- src/kr/or/dgit/bigdata/coffee/ui/list/CoffeeList2.java
+++ src/kr/or/dgit/bigdata/coffee/ui/list/CoffeeList2.java
@@ -1,24 +1,5 @@
 package kr.or.dgit.bigdata.coffee.ui.list;
 
-import javax.swing.JPanel;
-import javax.swing.JTable;
-import javax.swing.JScrollPane;
-import javax.swing.table.DefaultTableCellRenderer;
-import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableColumnModel;
-
-import kr.or.dgit.bigdata.coffee.dao.TableDao;
-import kr.or.dgit.bigdata.coffee.dto.ViewTable;
-
-import java.awt.BorderLayout;
-import javax.swing.JLabel;
-import java.awt.Font;
-import java.awt.GridLayout;
-import java.util.List;
-import java.util.Vector;
-
-import javax.swing.SwingConstants;
-import javax.swing.border.EmptyBorder;
 
 public class CoffeeList2 extends CoffeeList {
 
